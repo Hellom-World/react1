0d6f6aa2cc063d5f75d9d0d7f0385fb7adf27e9c
Hellom-World <hellom-mendes@hotmail.com>
2022-09-08 16:05:50 +0100

    Propriedades do react: refarotação e utilização de props

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './styles/Feed.css';
 
 import userIcon from './images/user.svg';
 import paperPlaneIcon from './images/paper-plane.svg';
-import clockIcon from './images/clock.svg';
-import emptyFolderIcon from './images/empty-folder.svg';
+
+import Feed from './components/feed'
 
 import {useState} from 'react';
 
@@ -56,47 +56,7 @@ function App() {
                 </div>
             </form>
             <main>
-                {posts.length == 0 && (
-                    <div className='feed-status'>
-                        <img src={emptyFolderIcon} alt="Empty folder"></img>
-                        
-                        <h1>Não encontramos nada</h1>
-                        <h2>Parece que voce e seus amigos não postaram nada. Começa a escrever um nova história!</h2>
-                    </div>
-                )}
-                {posts.length > 0 && ( 
-                <>
-                    <header>
-                            <h1>Seu Feed</h1>
-                            <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-                        </header>
-                        <section className='feed'>
-
-                        {posts.map((post) => (
-
-                        <article key={post.id}>
-                            <p>
-                            {post.content}
-                            </p>
-
-                            <footer>
-                                <div className='user-details'>
-                                    <img src={userIcon} alt="User"/>
-                                    <strong>{post.userName}</strong>
-                                </div>
-                                <div className='time'>
-                                    <img src={clockIcon} alt="Clock"></img>
-                                    <span>Publicado em {post.publishedAt.toLocaleDateString()}</span>
-                                </div>
-                            </footer>
-                        </article>
-                        ))}
-
-                        
-                    </section>
-                </>
-                )}
-                
+                <Feed posts={posts}/>
             </main>
         </div>
     );
