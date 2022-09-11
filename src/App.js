1327e5052aa0bbeafe170146369d0a02ab8fa9b3
Hellom-World <hellom-mendes@hotmail.com>
2022-09-11 20:16:33 +0100

    Componentizando o formulario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './styles/App.css';
-import './styles/PostForm.css';
-import './styles/Feed.css';
 
-import userIcon from './images/user.svg';
-import paperPlaneIcon from './images/paper-plane.svg';
-
-import Feed from './components/feed'
+import Feed from './components/Feed';
+import PostForm from './components/PostForm';
 
 import {useState} from 'react';
 
 function App() {
-    const [history, setHistory] = useState('');
-    const [userName, setUserName] = useState('');
     const [posts, setPosts] = useState([]);
 
     
-    function handleSubmit(event){
-        event.preventDefault();
-        
+    function handleSubmit({history, userName}){
         setPosts([
           ...posts,  
         {
@@ -26,35 +18,11 @@ function App() {
           userName: userName,
           publishedAt: new Date(),
         }])
-
-        setHistory('');
-        setUserName('');
     }
 
     return (
         <div className="wrapper">
-            <form className="post-form" onSubmit={handleSubmit}>
-                <input 
-                    value={history}
-                    placeholder="Escreva uma nova história..."
-                    onChange={(event) => setHistory(event.target.value)}
-                />
-                <div>
-                    <img src={userIcon} alt="userIcon" />
-
-                    <input 
-                        value={userName}
-                        placeholder="Digite seu nome..."
-                        onChange={(event) => setUserName(event.target.value)}
-                    />
-
-                    <button type="submit">
-                        <img src={paperPlaneIcon} alt="paper-plane" />
-                    
-                        Publicar
-                    </button>
-                </div>
-            </form>
+            <PostForm onSubmit={handleSubmit} />
             <main>
                 <Feed posts={posts}/>
             </main>
